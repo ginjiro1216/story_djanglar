fef87e59bb06b3e9d74b68dc9e4642ec28250eee
ginjiro1216 <{ginpoko12161216@gmail.com}>
2020-12-03 22:34:35 +0900

    first commit

---
+++ story/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Publisher(models.Model):
+
+    class Meta:
+        db_table = 'publisher'
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    class Meta:
+        db_table = 'stories'
+
+    author = models.CharField(max_length=25)
+    title = models.CharField(max_length=25)
+    story = models.TextField()
+    date = models.DateTimeField()
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
